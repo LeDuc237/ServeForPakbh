ffe0acba88de4a8036a7305ce514110816987da9
Duchel Decarte <Duchel Decarte>
2025-09-10 09:25:43 +0100

    update the index

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.use(cors({
     'http://localhost:5173', 
     'http://localhost:3000', 
     'https://pakbh.com',
+    'https://www.pakbh.com',
     'https://delicate-banoffee-384c86.netlify.app'
   ],
   credentials: true
